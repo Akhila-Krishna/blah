08aae0ef18f18a00d324d3456296373dcc2bd767
Akhila-Krishna <akhilack057@cet.ac.in>
2020-06-10 19:35:43 +0530

    Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Jumbotron, Container, Row, Col, Image, Button, Figure } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col, Image, Button, Form, Figure } from 'react-bootstrap';
 import './Home.css';
 
 
@@ -8,10 +8,27 @@ export default class Home extends Component{
     render() {
         return(
             <Container>
-                <h1>Hi, I am Akhila!</h1>
-                <Row className = "person-wrapper">
-                    <Image src = "assets/akhila.png" className = "profile-pic" />
-                </Row>
+                <h1>Login Here!</h1>
+                <Image src = "assets/logo.png" className = "right" />
+                <Col xs={6} className = "right">
+                <Form className = "bottom">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="danger" type="submit">
+                    Submit
+                    </Button>
+                    </Form>
+                </Col>
             </Container>
         )
     }
